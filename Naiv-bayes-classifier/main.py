54b695d1b990381c7108bbd19026d85763247943
debugger06 <debugger_06@yahoo.com>
2015-11-26 16:09:49 +0100

    naive bayes classifier

--- Naiv-bayes-classifier/main.py
+++ Naiv-bayes-classifier/main.py
@@ -21,5 +21,24 @@ new_day = ["ran","didn't wake up early"]
 
 def calc_y_probability(y_label,days):
     return len([d for d in days if d[1]==y_label])/float(len(days))
+def calc_ran_given_y(ran_label,y_label,days):
+    return len([d for d in days if d[0]==ran_label and d[1]==y_label])/float(len(days))
 
-print calc_y_probability("was tired",days)
+def calc_woke_up_early_given_y(woke_up_early_label,y_label,days):
+    return len([d for d in days if d[2]==woke_up_early_label and d[1]==y_label])/float(len(days))
+
+y = "was tired"
+prob_y = calc_y_probability(y,days)
+prob_ran_given_y = calc_ran_given_y(new_day[0],y,days)
+prob_woke_up_early_given_y = calc_woke_up_early_given_y(new_day[1],y,days)
+denom = len([d for d in days if d[0]==new_day[0] and d[2]==new_day[1]])/float(len(days))
+prob_Y_given_X = prob_y*prob_ran_given_y*prob_woke_up_early_given_y/denom
+print "probability that he was not tired" prob_Y_given_X
+
+y = "was not tired"
+prob_y = calc_y_probability(y,days)
+prob_ran_given_y = calc_ran_given_y(new_day[0],y,days)
+prob_woke_up_early_given_y = calc_woke_up_early_given_y(new_day[1],y,days)
+denom = len([d for d in days if d[0]==new_day[0] and d[2]==new_day[1]])/float(len(days))
+prob_Y_given_X = prob_y*prob_ran_given_y*prob_woke_up_early_given_y/denom
+print prob_Y_given_X
